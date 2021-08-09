2272ab43666537ca64fc67196c8f6bf1d048d426
shivam jain <shivam.j@chapter247.com>
2021-08-09 16:28:05 +0530

    remove unused

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,17 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(MenusTableSeeder::class);
-        //$this->call(UsersAndNotesSeeder::class);
-        /*
-        $this->call('UsersAndNotesSeeder');
-        $this->call('MenusTableSeeder');
-        $this->call('FolderTableSeeder');
-        $this->call('ExampleSeeder');
-        $this->call('BREADSeeder');
-        $this->call('EmailSeeder');
-        */
-
         $this->call([
             UsersAndNotesSeeder::class,
             MenusTableSeeder::class,
